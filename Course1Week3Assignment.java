a0558d565b46ec21a06be525abe80bacc81b2152
PradeC123 <123870956+PradeC123@users.noreply.github.com>
2023-03-16 22:06:17 +0530

    Course 1 Week 3 Java Implementation Question 1
    Done

--- Course1Week3Assignment.java
+++ Course1Week3Assignment.java
@@ -1,40 +1,56 @@
 import java.lang.reflect.Array;
 
 public class Course1Week3Assignment {
-/* 
-def coinchange(n):
-    # list of available coins
-    coin_change = [1,5,10]
-    # variable to keep track of number of coins used
-    count_coin = 0
-    # continue until n becomes zero
-    while (n != 0):
-        # if n is greater than or equal to the maximum coin value available
-        if n >= max(coin_change):
-            # add one to the coin count
-            count_coin += 1 
-            # subtract the maximum coin value from n
-            n -= max(coin_change)
-        else: 
-            # remove the maximum coin value from the list of available coins
-            coin_change.remove(max(coin_change))
-    # return the number of coins used to make change for n
-    return count_coin
-    */
     //-------------------------------------------------------------------------//
     // Question 1 : Money Change 
 
     // Finding the maximum element in an array
-    public static int maxcoin(int[] arr_coin){
+    public static int[] findMax(int[] arr){
+        int max = arr[0];
+        int index = 0;
+        for(int i = 0; i < arr.length; i++){
+            if (arr[i] > max){
+                max = arr[i];
+                index = i;
+            }
+        }
+        return new int[]{max,index};
     }
     // Removing the element from an array
-    public static int[] array_remove(int[] arr_remove){
-
+    public static int[] removeElement(int[] arr, int index){
+        int[] newArr = new int[arr.length -1];
+        for(int i = 0, j = 0; i < arr.length ; i++){
+            if (i != index){
+                newArr[j] = arr[i];
+                j++;
+            }
+        }
+        return newArr;
     }
+    // Findthe the coinchange need to implement. 
     public static int coinchange(int n) {
         int[] coin_change = {1,5,10};
-        return n;
-        
+        int count_coin = 0; 
+        while(n >= 0 && coin_change.length > 0){
+            int max_coin = findMax(coin_change)[0];
+            int max_index = findMax(coin_change)[1];
+            if (n >= max_coin){
+                count_coin += 1;
+                n -= max_coin;
+            }
+            else{
+                coin_change = removeElement(coin_change, max_index);
+            }
+        }
+        return count_coin;         
+    }
+    //-------------------------------------------------------------------------//
+    // Question 2 : Maximum Value of the Loot
+
+
+    //--------------TESTING THE CODE------------
+    public static void main(String[] args) {
+        System.out.println(coinchange(19));
     }
 
 }
