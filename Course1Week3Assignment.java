14d7f7db8d58bc09bd1f0e631546aed20566df67
PradeC123 <123870956+PradeC123@users.noreply.github.com>
2023-03-16 19:27:02 +0530

    Create Course1Week3Assignment.java
    Java programming

---
+++ Course1Week3Assignment.java
@@ -0,0 +1,40 @@
+import java.lang.reflect.Array;
+
+public class Course1Week3Assignment {
+/* 
+def coinchange(n):
+    # list of available coins
+    coin_change = [1,5,10]
+    # variable to keep track of number of coins used
+    count_coin = 0
+    # continue until n becomes zero
+    while (n != 0):
+        # if n is greater than or equal to the maximum coin value available
+        if n >= max(coin_change):
+            # add one to the coin count
+            count_coin += 1 
+            # subtract the maximum coin value from n
+            n -= max(coin_change)
+        else: 
+            # remove the maximum coin value from the list of available coins
+            coin_change.remove(max(coin_change))
+    # return the number of coins used to make change for n
+    return count_coin
+    */
+    //-------------------------------------------------------------------------//
+    // Question 1 : Money Change 
+
+    // Finding the maximum element in an array
+    public static int maxcoin(int[] arr_coin){
+    }
+    // Removing the element from an array
+    public static int[] array_remove(int[] arr_remove){
+
+    }
+    public static int coinchange(int n) {
+        int[] coin_change = {1,5,10};
+        return n;
+        
+    }
+
+}
