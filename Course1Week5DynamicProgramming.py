f4ab013cccd251d9b10de20962236ff9833e94d9
PradeC123 <123870956+PradeC123@users.noreply.github.com>
2023-03-31 22:45:45 +0530

    Update Course1Week5DynamicProgramming.py

--- Course1Week5DynamicProgramming.py
+++ Course1Week5DynamicProgramming.py
@@ -34,7 +34,31 @@ def minoperCal(n):
     operations.reverse()
     return dp_arr[-1], operations
 
+##------------------------------------------------------------
+## Question 3: Alignment game
+
+
+##------------------------------------------------------------
+## Question 4: Longest Common Subsequence of Two Sequences
+def longestcommonSeq(seq1,seq2):
+    n = len(seq1)
+    m = len(seq2)
+    dp = [[0 for j in range(m+1)] for i in range(n+1)]
+    print(dp)
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if seq1[i-1] == seq2[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+    return dp[n][m]
+##-----------------------------------------------------------------
+# Test Cases. Question 1, 2 and 4
 #coin_arr = [1,3,4]
 #W = 7
 #print("Min coin change", moneychangeDP(coin_arr,W))
-print(minoperCal(1000))
+#print(minoperCal(1000))
+seq1 = [1, 2, 3, 4, 5]
+seq2 = [2, 4, 5, 6]
+result = longestcommonSeq(seq1, seq2)
+print(result) # Output: 3
