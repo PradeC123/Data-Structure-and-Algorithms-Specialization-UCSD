22d43ae248a16960e95fd68de7ca51b79bdb75ed
PradeC123 <123870956+PradeC123@users.noreply.github.com>
2023-04-04 07:36:08 +0530

    Course1Week5
    Course1Week5. Question 4 and 5 done for C

--- Course1Week5SDynamicProgramming.c
+++ Course1Week5SDynamicProgramming.c
@@ -6,8 +6,15 @@ int min(int elem1, int elem2){
     return (elem1 > elem2) ? elem2 : elem1;
 }
 
+int max(int elem1, int elem2){
+    return (elem1 > elem2) ? elem1 : elem2;
+}
+
 int min3(int elem1, int elem2, int elem3){
     return min(elem1,min(elem2,elem3));
+}
+int max3(int elem1, int elem2, int elem3) {
+    return max(elem1,max(elem2,elem3));
 }
 
 // Question 1: Money Change
@@ -50,6 +57,7 @@ int mincalcDP(int num){
     }
     return dp[num];
 }
+
 // Question 3: Alignment Game
 int editDistance(char seq1[], int seq1n, char seq2[], int seq2n){
     //Initialization of the DP
@@ -79,6 +87,54 @@ int editDistance(char seq1[], int seq1n, char seq2[], int seq2n){
     return dp[0][0];
 }
 
+// Question 4: 
+int lcs2D(int seq1[], int len_seq1, int seq2[], int len_seq2){
+    int dp[len_seq1+1][len_seq2+1];
+    // Initialization of the DP array.
+    for(int i = 0; i < len_seq1+1; i++){
+        for(int j = 0; j < len_seq2+1; j++){
+            dp[i][j] = 0;
+        }
+    }
+    // Filling up the DP array. 
+    for(int i = 1; i < len_seq1 + 1; i++){
+        for(int j = 1; j < len_seq2 + 1; j++){
+            if(seq1[i] == seq2[j]){
+                dp[i][j] =  1 + dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+    }
+    return dp[len_seq1][len_seq2];
+}
+
+// Question 5: 
+int lcs3(int a[], int n, int b[], int m, int c[], int l) {
+    int dp[n+1][m+1][l+1];
+    int i, j, k;
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= m; j++) {
+            for (k = 0; k <= l; k++) {
+                dp[i][j][k] = 0;
+            }
+        }
+    }
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= m; j++) {
+            for (k = 1; k <= l; k++) {
+                if (a[i-1] == b[j-1] && b[j-1] == c[k-1]) {
+                    dp[i][j][k] = dp[i-1][j-1][k-1] + 1;
+                } else {
+                    dp[i][j][k] = max3(dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1]);
+                }
+            }
+        }
+    }
+    return dp[n][m][l];
+}
+// Test cases for the algorithm
 int main(){
     /*
     int coins[] = {1, 5, 10, 25};
